68007695fc9c0f1a3f89140c998bea2ce6510cb6
hexiongwu1995_local <hexiongwu1995@outlook.com>
2026-09-16 07:16:23 +0800

    成功绘制电场线

--- electric-field.js
+++ electric-field.js
@@ -2,7 +2,7 @@
 
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { constants } from 'fundamental-physical-constants';
+import { constants } from "fundamental-physical-constants";
 
 const dpi = window.devicePixelRatio;
 const canvas = document.querySelector("#electric-field-canvas");
@@ -20,7 +20,7 @@ scene.add(axesHelper);
 const gridHelper = new THREE.GridHelper(0.2, 10);
 scene.add(gridHelper);
 
-const particle1 = new THREE.Mesh(new THREE.SphereGeometry(0.002, 16, 16), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+const particle1 = new THREE.Mesh(new THREE.SphereGeometry(0.002, 16, 16), new THREE.MeshBasicMaterial({ color: 0xaaaaaa, opacity: 0.5, transparent: true }));
 particle1.position.set(0, 0, 0);
 scene.add(particle1);
 
@@ -35,13 +35,78 @@ const particle1Charge = 4.0e-8;
 const particle2Charge = -1.0e-8;
 const distance = 0.06;
 
-const electricForce = k * particle1Charge * particle2Charge / distance ** 2;
+const electricForce = (k * particle1Charge * particle2Charge) / distance ** 2;
 
 textElement.innerHTML = `静电力常数： ${k.toExponential(2)} N m^2 / C^2 <br> 两个电荷之间的静电力： ${electricForce.toExponential(2)} N `;
 
+// 生成球面采样点
+let surfacePoints = [];
+function generateSurfacePoints(particle, options = {}) {
+  const { stepNum = 10 } = options;
+  const radius = particle.geometry.parameters.radius;
+  for (let i = 0; i < stepNum; i++) {
+    for (let j = 0; j < stepNum; j++) {
+      let phi = (i * Math.PI) / stepNum;
+      let theta = (j * 2 * Math.PI) / stepNum;
+      const surfacePoint = new THREE.Vector3();
+      surfacePoint.setFromSphericalCoords(radius, phi, theta);
+      surfacePoints.push(surfacePoint);
+    }
+  }
+}
+
+generateSurfacePoints(particle1, { stepNum: 10 });
+
+// textElement.innerHTML += `<br>球面采样点数量：${surfacePoints.length}`;
+
+console.log(surfacePoints);
+console.log(particle1.position);
+
+for (let i = 0; i < surfacePoints.length; i++) {
+  const line = new THREE.Line(new THREE.BufferGeometry().setFromPoints([particle1.position.clone(), surfacePoints[i].clone()]), new THREE.LineBasicMaterial({ color: 0xff0000 }));
+  scene.add(line);
+}
+
+function getSpaceElectricField(point) {
+  const field = new THREE.Vector3();
+  // 来自particle1的电场
+  const distance1 = point.distanceTo(particle1.position);
+  const filed1 = point.clone()
+    .sub(particle1.position)
+    .normalize()
+    .multiplyScalar((k * particle1Charge) / distance1 ** 2);
+  field.add(filed1);
+  // 来自particle2的电场
+  const distance2 = point.distanceTo(particle2.position);
+  const filed2 = point.clone()
+    .sub(particle2.position)
+    .normalize()
+    .multiplyScalar((k * particle2Charge) / distance2 ** 2);
+  field.add(filed2);
+  return field;
+}
+
+function traceFieldLineFromSurface(stepLength, maxStepLength) {
+  for (let i = 0; i < surfacePoints.length; i++) {
+    let startPoint = surfacePoints[i].clone();
+    let direction = getSpaceElectricField(startPoint).normalize();
+    let currentPoint = startPoint.clone().add(direction.multiplyScalar(stepLength));
+    while (currentPoint.distanceTo(particle1.position) <= maxStepLength && currentPoint.distanceTo(particle2.position) >= particle2.geometry.parameters.radius) {
+      const line = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([startPoint.clone(), currentPoint.clone()]), 
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+      scene.add(line);
+      startPoint = currentPoint.clone();
+      direction = getSpaceElectricField(startPoint).normalize();
+      currentPoint = currentPoint.clone().add(direction.multiplyScalar(stepLength));
+    }
+  }
+}
+
+traceFieldLineFromSurface(0.005, 0.12);
 
 const camera = new THREE.PerspectiveCamera(75, canvasWidth / canvasHeight, 0.01, 10);
-camera.position.set(0.03, 0, 0.1);
+camera.position.set(0.03, 0.03, 0.1);
 // camera.lookAt(0.03, 0, 0);
 scene.add(camera);
 
